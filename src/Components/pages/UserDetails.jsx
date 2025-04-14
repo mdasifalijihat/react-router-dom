925f3d572cd792c50bc92d668d257cac4639b914
mdasifalijihat <mdasifalijihat@gmail.com>
2025-04-14 20:19:10 +0600

    add dynamic user data show

--- src/Components/pages/UserDetails.jsx
+++ src/Components/pages/UserDetails.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useLoaderData } from 'react-router';
 
 const UserDetails = () => {
+
+    const user = useLoaderData();
+    const {website, name} = user;
     return (
-        <div>
+        <div className='text-center font-bold gap-6 p-6'>
             <p> user details </p>
+            <p className='text-red-600'> website: {website }</p>
+            <h2> Name: {name}</h2>
         </div>
     );
 };
