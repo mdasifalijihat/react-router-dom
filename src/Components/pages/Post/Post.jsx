f3e0f9328f028030e1e6bab988a0552c24665b2f
mdasifalijihat <mdasifalijihat@gmail.com>
2025-04-14 21:13:13 +0600

    add navigate

--- src/Components/pages/Post/Post.jsx
+++ src/Components/pages/Post/Post.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 
 const Post = ({post}) => {
     const {id, title} =post;
+
+     const navigate = useNavigate();
+    const handleNavigate = () => {
+        navigate('/')
+    }
     return (
         <div>
             <div className='border p-4 text-red-500 rounded-2xl space-y-4'>
                 <h2>{id} </h2>
                 <p>{title}</p>
                 <Link className='border p-2 rounded-2xl text-black' to={`/posts/${id}`}> show details </Link>
+
+                <button onClick={handleNavigate} className='btn'> Details of: {id}</button>
             </div>
         </div>
     );
